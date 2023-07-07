9e76b02589e078b6d9dd45a6bf925f78b360b9a3
unknown <dandarabiancas@gmail.com>
2023-07-06 22:36:29 -0300

    lista de tarefas

--- src/App.js
+++ src/App.js
@@ -2,91 +2,85 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [input, setInput] = useState('');
-  const [tarefas, setTarefas] = useState([
-  ]);
+  const [task, setTask] = useState('');
+  const [time, setTime ] = useState('');
+  const [date,setDate ] = useState('');
+  const [tarefas, setTarefas] = useState([]);
 
   useEffect(() => {
-    const tarefasStorage = localStorage.getItem('@tarefas');
-
+    const tarefasStorage = localStorage.getItem('tarefas');
+  
     if (tarefasStorage) {
-      setTarefas(JSON.parse(tarefasStorage))
+      setTarefas(JSON.parse(tarefasStorage));
     }
-
+  
   }, []);
+  
 
   useEffect(() => {
-    localStorage.setItem('@tarefa', JSON.stringify('tarefas'))
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
   }, [tarefas]);
 
   function handleRegistrar(event) {
     event.preventDefault();
 
     const novaTarefa = {
-      palavra: input,
-      data: getCurrentDate(),
-      hora: getCurrentTime()
+      palavra: task,
+      data: date,
+      hora: time
     };
 
-    setTarefas([...tarefas, input]);
-    setInput('');
+    setTarefas([...tarefas, novaTarefa]);
+    setTask('');
   }
 
-  function getCurrentDate() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0');
-    const day = String(now.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  }
 
-  function getCurrentTime() {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    return `${hours}:${minutes}`;
-  }
 
   return (
     <div className='containerCenter'>
       <h1>Lista de tarefas!</h1>
       <div className='items'>
         <div className='inputs'>
-      <form onSubmit={handleRegistrar}>
-        <label>Tarefas:</label>
-        <input
-          placeholder='Digite uma tarefa'
+          <form onSubmit={handleRegistrar}>
+            <label>Tarefas:</label>
+            <input
+              placeholder='Digite uma tarefa'
+              value={task}
+              onChange={(event) => setTask(event.target.value)}
+            />
 
-          value={input}
-          onChange={(event) => setInput(event.target.value)}
-        />
+            <label htmlFor="hour">Hora:</label>
+            <input
+              id="hour"
+              type='time'
+              placeholder='Digite a hora da tarefa'
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+            />
 
-        <label for="hour">Hora:</label>
-         <input
-         id="hour"
-         type='time'
-         placeholder='Digite a hora da tarefa'
-         />
-        <label for="date">Date:</label>
-        <input
-        id="date"
-        type="date"
-         placeholder='Digite a data da tarefa'
-         />
-        <button type='submit'>Registrar</button>
-      </form>
+            <label htmlFor="date">Data:</label>
+            <input
+              id="date"
+              type="date"
+              placeholder='Digite a data da tarefa'
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+
+            <button type='submit'>Registrar</button>
+          </form>
+        </div>
+        <ul>
+          
+          {tarefas.map((tarefa, index) => (
+            <li key={index}>
+              <span>Tarefa: {tarefa.palavra}</span>
+              <span>Data: {tarefa.data}</span>
+              <span>Hora: {tarefa.hora}</span>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul>
-      {tarefas.map((tarefa, index) => (
-        <li key={index}>
-          <p>Tarefa: {tarefa.palavra}</p>
-          <p>Data: {tarefa.data}</p>
-          <p>Hora: {tarefa.hora}</p>
-        </li>
-        ))}
-      </ul>
-
-    </div>
     </div>
   );
 }
