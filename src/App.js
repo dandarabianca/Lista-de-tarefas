adbf4aa4e05c4a415087db3a4021cc9b1d8b754a
unknown <dandarabiancas@gmail.com>
2023-07-09 18:52:43 -0300

    lista de tarefas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [task, setTask] = useState('');
-  const [time, setTime ] = useState('');
-  const [date,setDate ] = useState('');
+  const [task, setTask] = useState("");
+  const [time, setTime] = useState("");
+  const [date, setDate] = useState("");
   const [tarefas, setTarefas] = useState([]);
+  const [tarefasProntas, setTarefasProntas] = useState([]);
 
   useEffect(() => {
-    const tarefasStorage = localStorage.getItem('tarefas');
-  
+    const tarefasStorage = localStorage.getItem("tarefas");
+
     if (tarefasStorage) {
       setTarefas(JSON.parse(tarefasStorage));
     }
-  
   }, []);
-  
 
   useEffect(() => {
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
   }, [tarefas]);
 
   function handleRegistrar(event) {
@@ -27,59 +26,96 @@ function App() {
     const novaTarefa = {
       palavra: task,
       data: date,
-      hora: time
+      hora: time,
     };
 
     setTarefas([...tarefas, novaTarefa]);
-    setTask('');
+    setTask("");
   }
 
 
+  function handleTask(novaTarefa) {
+    setTarefas((tarefas) => tarefas.filter((tarefa) => tarefa !== novaTarefa));
+    setTarefasProntas((tarefasProntas) => [...tarefasProntas, novaTarefa]);
+  }
 
   return (
-    <div className='containerCenter'>
-      <h1>Lista de tarefas!</h1>
-      <div className='items'>
-        <div className='inputs'>
+    <div className="containerCenter">
+      <div className="items">
+        <div className="inputs">
           <form onSubmit={handleRegistrar}>
             <label>Tarefas:</label>
             <input
-              placeholder='Digite uma tarefa'
+              className="inputsItems"
+              placeholder="Digite uma tarefa"
               value={task}
               onChange={(event) => setTask(event.target.value)}
             />
 
             <label htmlFor="hour">Hora:</label>
             <input
+              className="inputsItems"
               id="hour"
-              type='time'
-              placeholder='Digite a hora da tarefa'
+              type="time"
+              placeholder="Digite a hora da tarefa"
               value={time}
               onChange={(e) => setTime(e.target.value)}
             />
 
             <label htmlFor="date">Data:</label>
             <input
+              className="inputsItems"
               id="date"
               type="date"
-              placeholder='Digite a data da tarefa'
+              placeholder="Digite a data da tarefa"
               value={date}
               onChange={(e) => setDate(e.target.value)}
             />
 
-            <button type='submit'>Registrar</button>
+            <button className="buttonAdd" type="submit">
+              Adicionar
+            </button>
           </form>
         </div>
-        <ul>
-          
-          {tarefas.map((tarefa, index) => (
-            <li key={index}>
-              <span>Tarefa: {tarefa.palavra}</span>
-              <span>Data: {tarefa.data}</span>
-              <span>Hora: {tarefa.hora}</span>
-            </li>
-          ))}
-        </ul>
+        <div className="tarefasConteiner">
+          <h1>Lista de tarefas!</h1>
+          <h2>Tarefas para fazer: </h2>
+          <div className="tarefasMap">
+            <ul>
+              {tarefas.map((tarefa, index) => (
+                <li key={index}>
+                  <button type="submit">x</button>
+                  <span>Tarefa: {tarefa.palavra}</span>
+                  <span>Data: {tarefa.data}</span>
+                  <span>Hora: {tarefa.hora}</span>
+
+                  <input
+                    className="buttonStyle"
+                    type="checkbox"
+                    value={tarefa}
+                    onChange={() => handleTask(tarefa)}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="tarefasProntas">
+            <h2>Prontas: </h2>
+            {tarefasProntas && tarefasProntas.length > 0 ? (
+              tarefasProntas.map((tarefaPronta, index) => (
+                <div key={index}>
+                  <div className="styleTarefa">
+                  <span>Tarefa: {tarefaPronta.palavra}</span>
+                  <span>Data: {tarefaPronta.data}</span>
+                  <span>Hora: {tarefaPronta.hora}</span>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div>Não há tarefas prontas.</div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
